9acaccee629081f4ac12e65e6ac037764e6f2ae7
kamena <kamena.dacheva@gmail.com>
2015-09-07 14:58:17 +0300

    Client metabox with dropdown menu

--- clients_menu.php
+++ clients_menu.php
@@ -1,113 +1,121 @@
 <?php 
-add_action( 'init', 'add_submenu_clients' );
+class Client {
+    public function __construct() {
+        add_action( 'init', array( $this, 'add_submenu_clients' ));
+        add_action( 'add_meta_boxes', array( $this, 'cd_meta_box_add' ));
+        add_action( 'save_post', array( $this, 'add_client_fields'), 10, 2 );
+    }
 
-function add_submenu_clients() {
-    register_post_type( 'clients',
-        array(
-                'labels' => array(
-                    'name' => __( 'Добави/Виж клиенти' ),
-                    'singular_name' => __( 'Клиенти' ),
-                    'add_new' => 'Добави нов клиент',
-                    'add_new_item' => 'Добави нов клиент',
-                    'search_items' => 'Намери клиент',
-                ),
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array( 'title' ),
-        'show_in_menu' => 'edit.php?post_type=invoices'
-        )
-    );
+    public function add_submenu_clients() {
+        register_post_type( 'clients',
+            array(
+                    'labels' => array(
+                        'name' => __( 'Добави/Виж клиенти' ),
+                        'singular_name' => __( 'Клиенти' ),
+                        'add_new' => 'Добави нов клиент',
+                        'add_new_item' => 'Добави нов клиент',
+                        'search_items' => 'Намери клиент',
+                    ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array( 'title' ),
+            'show_in_menu' => 'edit.php?post_type=invoices'
+            )
+        );
 
-    // add_action('admin_menu', 'my_admin_menu'); 
-    // function my_admin_menu() { 
-    //     add_submenu_page('edit.php?post_type=invoices', 'Genre', 'Genre', 'manage_options', 'edit-tags.php?taxonomy=genre&post_type=invoices'); 
-    // }
-}
+        // add_action('admin_menu', 'my_admin_menu'); 
+        // function my_admin_menu() { 
+        //     add_submenu_page('edit.php?post_type=invoices', 'Genre', 'Genre', 'manage_options', 'edit-tags.php?taxonomy=genre&post_type=invoices'); 
+        // }
+    }
 
-add_action( 'add_meta_boxes', 'cd_meta_box_add' );
 
-function cd_meta_box_add() {
-    add_meta_box( 'my-meta-box-id', 
-        'Информация за купувача', 
-        'cd_meta_box_cb', 
-        'clients', 'normal', 'high' );
-}
+    public function cd_meta_box_add() {
+        add_meta_box( 'my-meta-box-id', 
+            'Информация за купувача', 
+            array( $this, 'cd_meta_box_cb' ), 
+            'clients', 'normal', 'high' );
+    }
 
-function cd_meta_box_cb( $client ) {
+    public function cd_meta_box_cb( $client ) {
 
-    $company_name = esc_html( get_post_meta( $client->ID, 'company_name', true ) );
-    $company_city = esc_html( get_post_meta( $client->ID, 'company_city', true) );
-    $company_address = esc_html( get_post_meta( $client->ID, 'company_address', true ) );
-    $company_id = esc_html( get_post_meta( $client->ID, 'company_id', true ) );
-    $responsible_person = esc_html( get_post_meta( $client->ID, 'responsible_person', true ) );
-    $client_name = esc_html( get_post_meta( $client->ID, 'client_name', true ) );  
-    $company_mail = esc_html( get_post_meta( $client->ID, 'company_mail', true ) );
-?>
+        $company_name = esc_html( get_post_meta( $client->ID, 'company_name', true ) );
+        $company_city = esc_html( get_post_meta( $client->ID, 'company_city', true) );
+        $company_address = esc_html( get_post_meta( $client->ID, 'company_address', true ) );
+        $company_id = esc_html( get_post_meta( $client->ID, 'company_id', true ) );
+        $responsible_person = esc_html( get_post_meta( $client->ID, 'responsible_person', true ) );
+        $client_name = esc_html( get_post_meta( $client->ID, 'client_name', true ) );  
+        $company_mail = esc_html( get_post_meta( $client->ID, 'company_mail', true ) );
+    ?>
 
-    <div>
-        <label>Име на фирмата</label>
-        <div><input name="user_information_company_name" type="text" value="<?php echo $company_name; ?>" size="8"></div>
-    </div>
+        <div>
+            <label>Име на фирмата</label>
+            <div><input name="user_information_company_name" type="text" value="<?php echo $company_name; ?>" size="8"></div>
+        </div>
 
-    <div>
-        <label>Град</label>
-        <div><input name="user_information_company_city" type="text" value="<?php echo $company_city; ?>" size="8"</div>
-    </div>
+        <div>
+            <label>Град</label>
+            <div><input name="user_information_company_city" type="text" value="<?php echo $company_city; ?>" size="8"</div>
+        </div>
 
-    <div>
-        <label>Адрес на фирмата</label>
-        <div><textarea name="user_information_company_address" rows="5" cols="50" value="<?php echo $company_address; ?>"></textarea></div>
-    </div>
+        <div>
+            <label>Адрес на фирмата</label>
+            <div><textarea name="user_information_company_address" rows="5" cols="50" value="<?php echo $company_address; ?>"></textarea></div>
+        </div>
 
-    <div>
-        <label>ЕИК/Булстат</label>
-        <div><input name="user_information_company_id" type="text" value="<?php echo $company_id; ?>" size="8"></div>
-    </div>
+        <div>
+            <label>ЕИК/Булстат</label>
+            <div><input name="user_information_company_id" type="text" value="<?php echo $company_id; ?>" size="8"></div>
+        </div>
 
-    <div>
-        <label>МОЛ</label>
-        <div><input name="user_information_responsible_person" type="text" value="<?php echo $responsible_person; ?>" size="8"></div>
-    </div>
+        <div>
+            <label>МОЛ</label>
+            <div><input name="user_information_responsible_person" type="text" value="<?php echo $responsible_person; ?>" size="8"></div>
+        </div>
 
-    <div>
-        <label>Име на получател</label>
-        <div><input name="user_information_client_name" type="text" value="<?php echo $client_name; ?>" size="8"></div>
-    </div>
+        <div>
+            <label>Име на получател</label>
+            <div><input name="user_information_client_name" type="text" value="<?php echo $client_name; ?>" size="8"></div>
+        </div>
 
-    <hr>
+        <hr>
 
-    <div>
-        <label>E-mail на фирмата</label>
-        <div><input name="user_information_company_mail" type="email" spellcheck="false" value="<?php echo $company_mail; ?>" maxlength="255"> </div>
-    </div>
-<?php
-}
+        <div>
+            <label>E-mail на фирмата</label>
+            <div><input name="user_information_company_mail" type="email" spellcheck="false" value="<?php echo $company_mail; ?>" maxlength="255"> </div>
+        </div>
+    <?php
+    }
 
-add_action( 'save_post', 'add_client_fields', 10, 2 );
 
-function add_client_fields( $client_id, $client ) {
-    if ( $client->post_type == 'clients' ) {
-        if ( isset( $_POST['user_information_company_name'] ) && $_POST['user_information_company_name'] != '' ) {
-            update_post_meta( $client_id, 'company_name', $_POST['user_information_company_name'] );
-        }
-        if ( isset( $_POST['user_information_company_city'] ) && $_POST['user_information_company_city'] != '' ) {
-            update_post_meta( $client_id, 'company_city', $_POST['user_information_company_city'] );
-        }
-        if ( isset( $_POST['user_information_company_address'] ) && $_POST['user_information_company_address'] != '' ) {
-            update_post_meta( $client_id, 'company_address', $_POST['user_information_company_address'] );
-        }
-        if ( isset( $_POST['user_information_company_id'] ) && $_POST['user_information_company_id'] != '' ) {
-            update_post_meta( $client_id, 'company_id', $_POST['user_information_company_id'] );
-        }
-        if ( isset( $_POST['user_information_responsible_person'] ) && $_POST['user_information_responsible_person'] != '' ) {
-            update_post_meta( $client_id, 'responsible_person', $_POST['user_information_responsible_person'] );
-        }
-        if ( isset( $_POST['user_information_client_name'] ) && $_POST['user_information_client_name'] != '' ) {
-            update_post_meta( $client_id, 'client_name', $_POST['user_information_client_name'] );
-        }
-        if ( isset( $_POST['user_information_company_mail'] ) && $_POST['user_information_company_mail'] != '' ) {
-            update_post_meta( $client_id, 'company_mail', $_POST['user_information_company_mail'] );
+    public function add_client_fields( $client_id, $client ) {
+        if ( $client->post_type == 'clients' ) {
+            if ( isset( $_POST['user_information_company_name'] ) && $_POST['user_information_company_name'] != '' ) {
+                update_post_meta( $client_id, 'company_name', $_POST['user_information_company_name'] );
+            }
+            if ( isset( $_POST['user_information_company_city'] ) && $_POST['user_information_company_city'] != '' ) {
+                update_post_meta( $client_id, 'company_city', $_POST['user_information_company_city'] );
+            }
+            if ( isset( $_POST['user_information_company_address'] ) && $_POST['user_information_company_address'] != '' ) {
+                update_post_meta( $client_id, 'company_address', $_POST['user_information_company_address'] );
+            }
+            if ( isset( $_POST['user_information_company_id'] ) && $_POST['user_information_company_id'] != '' ) {
+                update_post_meta( $client_id, 'company_id', $_POST['user_information_company_id'] );
+            }
+            if ( isset( $_POST['user_information_responsible_person'] ) && $_POST['user_information_responsible_person'] != '' ) {
+                update_post_meta( $client_id, 'responsible_person', $_POST['user_information_responsible_person'] );
+            }
+            if ( isset( $_POST['user_information_client_name'] ) && $_POST['user_information_client_name'] != '' ) {
+                update_post_meta( $client_id, 'client_name', $_POST['user_information_client_name'] );
+            }
+            if ( isset( $_POST['user_information_company_mail'] ) && $_POST['user_information_company_mail'] != '' ) {
+                update_post_meta( $client_id, 'company_mail', $_POST['user_information_company_mail'] );
+            }
         }
     }
+
 }
+
+new Client();
+
 ?>
--- kdwp-invoice.php
+++ kdwp-invoice.php
@@ -12,120 +12,127 @@ include( plugin_dir_path( __FILE__ ) . 'custom_screen_options.php');
 include( plugin_dir_path( __FILE__ ) . 'clients_custom_screen_options.php');
 include( plugin_dir_path( __FILE__ ) . 'clients_menu.php');
 
-add_action( 'admin_enqueue_scripts', 'register_admin_plugin_scripts' );
+class KDWPinvoice {
 
-function register_admin_plugin_scripts() {
-    wp_register_style( 'custom_wp_admin_css', plugins_url('/css/client_form.css', __FILE__ ), false, '1.0.0' );
-    wp_enqueue_style( 'custom_wp_admin_css' );
-}
+    public function __construct() {
+        add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_plugin_scripts' ));
+        add_action( 'init', array( $this, 'create_invoice' ));
+        add_action( 'admin_init', array( $this, 'my_admin' ));
+        add_action( 'save_post', array( $this, 'add_invoice_fields'), 10, 2 );
+        add_filter( 'template_include', array( $this, 'include_template_function', 1 ));
+    }
 
-add_action( 'init', 'create_invoice' );
+    public function register_admin_plugin_scripts() {
+        wp_register_style( 'custom_wp_admin_css', plugins_url('/css/client_form.css', __FILE__ ), false, '1.0.0' );
+        wp_enqueue_style( 'custom_wp_admin_css' );
+    }
 
-function create_invoice() {
-    register_post_type( 'invoices',
-        array(
-            'labels' => array( 
-                'name' => 'Book Annotations',
-                'singular_name' => 'Book Annotation',
-                'add_new' => 'Add New Annotation',
-                'add_new_item' => 'Add New Book Annotation',
-                'edit' => 'Edit',
-                'edit_item' => 'Edit Book Annotation',
-                'new_item' => 'New Book Annotation',
-                'view' => 'View',
-                'view_item' => 'View Book Annotation',
-                'search_items' => 'Search Book Annotations',
-                'not_found' => 'No Book Annotations found',
-                'not_found_in_trash' => 'No Book Annotations found in Trash',
-                'parent' => 'Parent Book Annotation'
-            ),
- 
-            'public' => true,
-            'menu_position' => 15,
-            'supports' => array( 'title', 'editor', 'comments', 'thumbnail' ),
-            'taxonomies' => array( '' ),
-            'menu_icon' => plugins_url( 'images/invoice-icon.png', __FILE__ ),
-            'has_archive' => true
-        )
-    );
+    public function create_invoice() {
+        register_post_type( 'invoices',
+            array(
+                'labels' => array( 
+                    'name' => 'Book Annotations',
+                    'singular_name' => 'Book Annotation',
+                    'add_new' => 'Add New Annotation',
+                    'add_new_item' => 'Add New Book Annotation',
+                    'edit' => 'Edit',
+                    'edit_item' => 'Edit Book Annotation',
+                    'new_item' => 'New Book Annotation',
+                    'view' => 'View',
+                    'view_item' => 'View Book Annotation',
+                    'search_items' => 'Search Book Annotations',
+                    'not_found' => 'No Book Annotations found',
+                    'not_found_in_trash' => 'No Book Annotations found in Trash',
+                    'parent' => 'Parent Book Annotation'
+                ),
+     
+                'public' => true,
+                'menu_position' => 15,
+                'supports' => array( 'title', 'editor', 'comments', 'thumbnail' ),
+                'taxonomies' => array( '' ),
+                'menu_icon' => plugins_url( 'images/invoice-icon.png', __FILE__ ),
+                'has_archive' => true
+            )
+        );
 
-	register_taxonomy( 'Genres', array('invoices'), 
-		array(
-			'hierarchical' => true,
-			'label' => 'Genres',
-			'singular_label' => 'Genre',
-			'rewrite' => true
-		)
-	);
+        register_taxonomy( 'Genres', array('invoices'), 
+            array(
+                'hierarchical' => true,
+                'label' => 'Genres',
+                'singular_label' => 'Genre',
+                'rewrite' => true
+            )
+        );
 
-}
-
-add_action( 'admin_init', 'my_admin' );
-
-function my_admin() {
-    add_meta_box( 'invoice_meta_box',
-        'Movie Review Details',
-        'display_invoice_meta_box',
-        'invoices', 'normal', 'high'
-    );
-}
-
-function display_invoice_meta_box( $invoice ) {
-    $author = esc_html( get_post_meta( $invoice->ID, 'author', true ) );
-    $book_rating = intval( get_post_meta( $invoice->ID, 'book_rating', true ) );
-    ?>
-    <table>
-        <tr>
-            <td style="width: 100%">Author</td>
-            <td><input type="text" size="80" name="invoice_author" value="<?php echo $author; ?>" /></td>
-        </tr>
-        <tr>
-            <td style="width: 150px">Book Rating</td>
-            <td>
-                <select style="width: 100px" name="invoice_rating">
-                <?php
-                // Generate all items of drop-down list
-                for ( $rating = 5; $rating >= 1; $rating -- ) {
-                ?>
-                    <option value="<?php echo $rating; ?>" <?php echo selected( $rating, $book_rating ); ?>>
-                    <?php echo $rating; ?> stars <?php } ?>
-                </select>
-            </td>
-        </tr>
-    </table>
-    <?php
-}
+    }
 
 
-add_action( 'save_post', 'add_invoice_fields', 10, 2 );
+    public function my_admin() {
+        add_meta_box( 'dropdown-client', 'Choose client', array( $this, 'client_metabox' ), 'invoices', 'normal', 'high' );
+    }
 
-function add_invoice_fields( $invoice_id, $invoice ) {
-    if ( $invoice->post_type == 'invoices' ) {
-        if ( isset( $_POST['invoice_author'] ) && $_POST['invoice_author'] != '' ) {
-            update_post_meta( $invoice_id, 'author', $_POST['invoice_author'] );
-        }
-        if ( isset( $_POST['invoicew_rating'] ) && $_POST['invoice_rating'] != '' ) {
-            update_post_meta( $invoice_id, 'book_rating', $_POST['invoice_rating'] );
+    public function client_metabox( $post ) {
+
+        $args = array(
+            'posts_per_page'   => 200,
+            'offset'           => 0,
+            'category'         => '',
+            'category_name'    => '',
+            'orderby'          => 'date',
+            'order'            => 'DESC',
+            'include'          => '',
+            'exclude'          => '',
+            'meta_key'         => '',
+            'meta_value'       => '',
+            'post_type'        => 'clients',
+            'post_mime_type'   => '',
+            'post_parent'      => '',
+            'author'       => '',
+            'post_status'      => 'publish',
+            'suppress_filters' => true 
+        ); 
+
+        $chosen_client = esc_html( get_post_meta( $post->ID, 'chosen_client', true ));
+        $all_clients = get_posts( $args );
+?>
+        <p>
+            <label>Client: </label>
+            <select name="the_client">
+                <option> </option>
+                 <?php  foreach ($all_clients as $client): ?>
+                    <option value="<?php echo $client->ID; ?>"<?php echo selected( $client->ID, $chosen_client ); ?>> 
+                        <?php echo $client->post_title; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </p>
+<?php   
+    }
+
+    public function add_invoice_fields( $invoice_id, $invoice ) {
+        if ( isset( $_POST['the_client'] ) && $_POST['the_client'] != '' ) {
+            update_post_meta( $invoice_id, 'chosen_client', $_POST['the_client'] );
         }
     }
-}
 
-add_filter( 'template_include', 'include_template_function', 1 );
-
-function include_template_function( $template_path ) {
-    if ( get_post_type() == 'invoices' ) {
-        if ( is_single() ) {
-            // checks if the file exists in the theme first,
-            // otherwise serve the file from the plugin
-            if ( $theme_file = locate_template( array ( 'single-invoces.php' ) ) ) {
-                $template_path = $theme_file;
-            } else {
-                $template_path = plugin_dir_path( __FILE__ ) . '/single-invoces.php';
+    public function include_template_function( $template_path ) {
+        if ( get_post_type() == 'invoices' ) {
+            if ( is_single() ) {
+                // checks if the file exists in the theme first,
+                // otherwise serve the file from the plugin
+                if ( $theme_file = locate_template( array ( 'single-invoces.php' ) ) ) {
+                    $template_path = $theme_file;
+                } else {
+                    $template_path = plugin_dir_path( __FILE__ ) . '/single-invoces.php';
+                }
             }
         }
+
+        return $template_path;
     }
 
-    return $template_path;
 }
+
+new KDWPinvoice();  
 
 ?>

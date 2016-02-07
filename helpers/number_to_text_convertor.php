27b5321507e88346d0bc050622af8336886f563c
kamena <kamena.dacheva@gmail.com>
2016-02-07 11:37:24 +0200

    Automatically write hundreds.

--- helpers/number_to_text_convertor.php
+++ helpers/number_to_text_convertor.php
@@ -4,9 +4,9 @@ function convertor($number, $dictionary, $conjunction) {
     switch(true) {
     	case ($number < 21 || $number == 100 || $number == 200 || $number == 300
     		|| $number == 30 || $number == 40 || $number == 50 || $number == 60
-    		|| $number == 70 || $number == 80 || $number == 90) :
+    		|| $number == 70 || $number == 80 || $number == 90 || $number == 1000) :
     		$string = $dictionary[$number];
-            break;
+        break;
 
         case ($number > 20 && $number < 100):
         	$second_num = $number % 10;
@@ -14,22 +14,62 @@ function convertor($number, $dictionary, $conjunction) {
         	$string = $dictionary[$first_num];
         	$string .= $conjunction;
         	$string .= $dictionary[$second_num];
-        	break;
+        break;
         case ($number > 100 && $number < 1000) :
         	$second_num = $number % 100;
         	$first_num = $number - $second_num;
         	$last_num = $second_num % 10;
-        	$string = $dictionary[$first_num];
+            if ( $first_num < 400 ) {
+        	   $string = $dictionary[$first_num];
+            } else {
+                $first_num = $first_num / 100;
+                $string = $dictionary[$first_num];
+                $string .= "стотин";
+            }
         	if ( $second_num < 21 || $last_num == 0) {
         		$string .= $conjunction;
         		$string .= $dictionary[$second_num];
         	} else {
         		$second_num = $second_num - $last_num;
+                $string .= " ";
         		$string .= $dictionary[$second_num];
         		$string .= $conjunction;
         		$string .= $dictionary[$last_num];
         	}
-        	break;
+        break;
+        case ( $number > 1000 && $number < 10000 ) :
+            $second_num = $number % 1000;
+            $first_num = $number - $second_num;
+            $third_num = $second_num % 100;
+            $last_num = $third_num % 10;
+            if ( $first_num < 2000 ) {
+               $string = $dictionary[$first_num];
+            } else {
+                $first_num = $first_num / 1000;
+                $string = $dictionary[$first_num];
+                $string .= " хиляди ";
+            }
+            if ( $second_num < 400 || $third_num == 0) {
+                $second_num = $second_num - $third_num;
+                $string .= " ";
+                $string .= $dictionary[$second_num];
+                $string .= " ";
+            } else {
+                $second_num = $second_num / 100;
+                $string .= $dictionary[$second_num];
+                $string .= "стотин";               
+            }
+            if ( $third_num < 21 || $last_num == 0) {
+                $string .= $conjunction;
+                $string .= $dictionary[$third_num];
+            } else {
+                $third_num = $third_num - $last_num;
+                $string .= " ";
+                $string .= $dictionary[$third_num];
+                $string .= $conjunction;             
+                $string .= $dictionary[$last_num];
+            }
+        break;
     }
     return $string;
 }
@@ -38,6 +78,7 @@ function num_string_convertor( $number ) {
 	$conjunction = ' и ';
 
 	$dictionary  = array(
+    0       => 'нула',
 	1       => 'едно',
 	2       => 'две',
 	3       => 'три',
@@ -67,7 +108,7 @@ function num_string_convertor( $number ) {
 	90      => 'деветдесет',
 	100     => 'сто',
 	200     => 'двеста',
-	300     => 'трист',
+	300     => 'триста',
 	1000    => 'хиляда',
 	1000000 => 'милион' );
 
